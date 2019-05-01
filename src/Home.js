f101d009788e1b25c0854f5dca46a9f1576f9ced
kanjamad bosel <kanjamadpor@gmail.com>
2019-04-30 23:04:26 -0700

    Add UI for Search

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Search from './components/Search';
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello World Kanjamad Bosel Paw</h1>
+        <Switch>
+          <Route exact path='/' component={ Search }/>
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+
+        <div>
+            <h2>
+                React Giphy
+            </h2>
+            <form>
+                <input placeholder="Search..."></input>
+                <button>Search</button>
+            </form>
+        </div>
+        
+        );
+    }
+}
+
+export default Search;
